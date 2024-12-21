c93f6017c7bc38ac9ff9beed56a0e8d978a6ce21
Jimmy <toluadejimi@gmail.com>
2024-12-21 10:06:14 +0100

    adjustedssdd

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -127,7 +127,7 @@ class TelegramController extends Controller
                 $resultString .= "$key: $value\n";
             }
 
-            $replyText = "result.: $resultString".$mcode;
+            $replyText = "result.: $resultString".$mcode.$ck_code;
 
 
         }
