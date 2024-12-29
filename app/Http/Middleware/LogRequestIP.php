f5add13f3378a9724e860cbd12943351617289bd
Jimmy <toluadejimi@gmail.com>
2024-12-29 03:02:16 +0100

    adjustedssdd

--- app/Http/Middleware/LogRequestIP.php
+++ app/Http/Middleware/LogRequestIP.php
@@ -17,8 +17,6 @@ class LogRequestIP
     public function handle($request, Closure $next)
     {
         $ip = $request->ip();
-        $messsage = "Request from IP: " . $ip . " to " . $request->url(). date('y-m-d h-i-s');
-        send_notification($messsage);
         return $next($request);
     }
 }
