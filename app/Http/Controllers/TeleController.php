6aea5c4505be262fb7aa1f3cddf0351fe4d88541
Jimmy <toluadejimi@gmail.com>
2025-02-07 20:47:59 +0100

    adjustedssdd

--- app/Http/Controllers/TeleController.php
+++ app/Http/Controllers/TeleController.php
@@ -44,8 +44,383 @@ class TeleController extends Controller
             $this->sendMessage($chatId, "Enter your transaction reference to check status.");
         } elseif (strpos($text, 'help') !== false) {
             $this->sendMessage($chatId, "How can I assist you?");
-        } else {
-            $this->sendMenu($chatId);
+        } elseif (strlen($text) >= 10 && stripos($text, '961') === 0) {
+            $title = $text;
+            $trx = Transfertransaction::where('account_no', $title)->first() ?? null;
+            if ($trx) {
+                $pref = $trx->ref;
+                $amount = number_format($trx->amount);
+                $email = $trx->email;
+                $date = $trx->created_at;
+                $sitename = Webkey::where('key', $trx->key)->first()->site_name ?? null;
+
+
+                $verify = verifypelpaytelegram($pref);
+
+                $cc = json_encode($verify);
+//               send_notification($cc);
+
+                if (!is_array($verify)) {
+                    $replyText = "Error: Unexpected response format.";
+
+                } else {
+                    switch ($verify['code']) {
+                        case 0:
+                            $replyText = "Account No: $title | still pending 🥺\n\n" .
+                                "We are sorry for any inconveniences!,\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n" .
+
+                                "I will keep notifying the bank about the transaction but if you can wait, you can file a dispute from your bank app";
+                            break;
+                        case 9:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n"
+                                . "If you have been debited, Please raise a dispute for reversal on your bank app";
+                            break;
+                        case 4:
+                            $replyText = "Account No: $title | already been funded ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+                        case 5:
+                            $replyText = "Account No: $title | part payment received. 🔄\n";
+                            break;
+
+                        case 2:
+                            $replyText = "Account No: $title | Transaction Completed ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+                        default:
+                            $replyText = "Account No: $title | processing resolve 🔄\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "code:$cc";
+                            break;
+                    }
+                }
+            } else {
+                $replyText = "Account no: $title | not found ❌\n"
+                    . "Please verify the Account No and try again.";
+            }
+
+        } elseif (strlen($text) >= 10 && stripos($text, '603') === 0) {
+            $title = $text;
+            $trx = Transfertransaction::where('account_no', $title)->first() ?? null;
+            if ($trx) {
+                $pref = $trx->ref;
+                $amount = number_format($trx->amount);
+                $email = $trx->email;
+                $date = $trx->created_at;
+                $sitename = Webkey::where('key', $trx->key)->first()->site_name ?? null;
+
+                $verify = verify_telegram_payment_woven($title);
+                $cc = json_encode($verify);
+
+                if (!is_array($verify)) {
+                    $replyText = "Error: Unexpected response format.";
+
+                } else {
+                    switch ($verify['code']) {
+                        case 0:
+                            $replyText = "Account No: $title | still pending 🥺\n\n" .
+                                "We are sorry for any inconveniences!,\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n" .
+
+                                "I will keep notifying the bank about the transaction but if you can wait, you can file a dispute from your bank app";
+                            break;
+                        case 9:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n"
+                                . "If you have been debited, Please contact your bank for reversal";
+                            break;
+
+                        case 6:
+                            $replyText = "Account No: $title | Transaction reversed 🔄\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n"
+                                . "Transaction has been successfully reversed back to your account";
+                            break;
+
+
+                        case 4:
+                            $replyText = "Account No: $title | already been funded ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+
+                        case 5:
+                            $replyText = "Account No: $title | part payment received. 🔄\n";
+                            break;
+
+                        case 2:
+                            $replyText = "Account No: $title | Transaction Completed ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+                        default:
+                            $replyText = "Account No: $title | processing resolve 🔄\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "code:$cc";
+                            break;
+                    }
+                }
+            } else {
+                $verify = verify_telegram_payment_woven($title);
+                $cc = json_encode($verify);
+
+                if (!is_array($verify)) {
+                    $replyText = $title . "| " . $cc . "Error: Unexpected response format.";
+                } else {
+                    switch ($verify['code']) {
+
+                        case 9:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "If you have been debited, Please contact your bank for reversal";
+                            break;
+                        case 6:
+                            $replyText = "Account No: $title | Transaction reversed 🔄\n\n"
+                                . "Transaction has been successfully reversed back to your account";
+                            break;
+
+                        default:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "If you have been debited, Please contact your bank for reversal";
+                            break;
+                    }
+                }
+            }
+
+        } elseif (strlen($text) >= 10 && stripos($text, '500') === 0) {
+            $title = $text;
+            $trx = Transfertransaction::where('account_no', $title)->first() ?? null;
+            if ($trx) {
+                $pref = $trx->ref;
+                $amount = number_format($trx->amount);
+                $email = $trx->email;
+                $date = $trx->created_at;
+                $sitename = Webkey::where('key', $trx->key)->first()->site_name ?? null;
+
+                $verify = verify_telegram_payment_woven($title);
+                $cc = json_encode($verify);
+
+                if (!is_array($verify)) {
+                    $replyText = $title . "| " . $cc . "Error: Unexpected response format.";
+
+                } else {
+                    switch ($verify['code']) {
+                        case 0:
+                            $replyText = "Account No: $title | still pending 🥺\n\n" .
+                                "We are sorry for any inconveniences!,\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n" .
+
+                                "I will keep notifying the bank about the transaction but if you can wait, you can file a dispute from your bank app";
+                            break;
+                        case 9:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n"
+                                . "If you have been debited, Please contact your bank for reversal";
+                            break;
+
+                        case 6:
+                            $replyText = "Account No: $title | Transaction reversed 🔄\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n"
+                                . "Transaction has been successfully reversed back to your account";
+                            break;
+
+
+                        case 4:
+                            $replyText = "Account No: $title | already been funded ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+
+                        case 5:
+                            $replyText = "Account No: $title | part payment received. 🔄\n";
+                            break;
+
+                        case 2:
+                            $replyText = "Account No: $title | Transaction Completed ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+                        default:
+                            $replyText = "Account No: $title | processing resolve 🔄\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "code:$cc";
+                            break;
+                    }
+                }
+            } else {
+
+                $verify = verify_telegram_payment_woven($title);
+                $cc = json_encode($verify);
+
+                if (!is_array($verify)) {
+                    $replyText = $title . "| " . $cc . "Error: Unexpected response format.";
+
+                } else {
+                    switch ($verify['code']) {
+                        case 9:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "If you have been debited, Please contact your bank for reversal";
+                            break;
+                        case 6:
+                            $replyText = "Account No: $title | Transaction reversed 🔄\n\n"
+                                . "Transaction has been successfully reversed back to your account";
+                            break;
+
+                        default:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "If you have been debited, Please contact your bank for reversal";
+                            break;
+                    }
+                }
+
+                $replyText = "Account no: $title | not found ❌\n"
+                    . "Please verify the Account No and try again.";
+            }
+
+        } elseif (strlen($text) >= 10 && stripos($text, '558') === 0) {
+            $title = $text;
+            $trx = Transfertransaction::where('account_no', $title)->first() ?? null;
+            if ($trx) {
+                $pref = $trx->account_no;
+                $amount = number_format($trx->amount);
+                $email = $trx->email;
+                $date = $trx->created_at;
+                $sitename = Webkey::where('key', $trx->key)->first()->site_name ?? null;
+
+
+                $verify = verifypsbtelegram($pref);
+
+                $cc = json_encode($verify);
+//               send_notification($cc);
+
+                if (!is_array($verify)) {
+                    $replyText = "Error: Unexpected response format.";
+
+                } else {
+                    switch ($verify['code']) {
+                        case 0:
+                            $replyText = "Account No: $title | still pending 🥺\n\n" .
+                                "We are sorry for any inconveniences!,\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n" .
+
+                                "I will keep notifying the bank about the transaction but if you can wait, you can file a dispute from your bank app";
+                            break;
+                        case 9:
+                            $replyText = "Account No: $title | Failed ❌\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount\n\n"
+                                . "If you have been debited, Please raise a dispute for reversal on your bank app";
+                            break;
+                        case 4:
+                            $replyText = "Account No: $title | already been funded ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+                        case 5:
+                            $replyText = "Account No: $title | part payment received. 🔄\n";
+                            break;
+
+                        case 2:
+                            $replyText = "Account No: $title | Transaction Completed ✅\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "Amount: $amount";
+                            break;
+
+                        default:
+                            $replyText = "Account No: $title | processing resolve 🔄\n\n"
+                                . "Transaction Details:\n"
+                                . "Email: $email\n"
+                                . "Date/Time: $date\n"
+                                . "Website: $sitename\n"
+                                . "code:$cc";
+                            break;
+                    }
+                }
+
+            } else {
+                $this->sendMenu($chatId);
+            }
         }
     }
 
