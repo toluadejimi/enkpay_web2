9813533adcf369f5d0b7a09bb335e9a2182d62d6
Jimmy <toluadejimi@gmail.com>
2025-05-14 00:05:09 +0100

    adjustedssdd

--- app/Http/Controllers/PalmpayController.php
+++ app/Http/Controllers/PalmpayController.php
@@ -160,12 +160,11 @@ class PalmpayController extends Controller
                 ));
 
                 $var = curl_exec($curl);
+                curl_close($curl);
+                $var = json_decode($var);
 
                 $message = "push trx===>".json_encode($var);
                 Log::info($message);
-
-                curl_close($curl);
-                $var = json_decode($var);
 
 
 
